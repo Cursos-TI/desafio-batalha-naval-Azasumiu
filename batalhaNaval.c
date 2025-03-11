f113b99e18fe9c7621333915b46060373ed697c4
Azasumiu <azasumiu@gmail.com>
2025-03-11 01:15:56 +0000

    Novato/Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,15 +5,30 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+  //  N = NAVIO, 0 = MAR
+    char *tabuleiro[10][10] = {
+            {"N", "N", "N", "0", "0", "0", "0", "0", "N", "0"},
+            {"0", "0", "0", "0", "0", "0", "0", "N", "0", "0"},
+            {"0", "0", "0", "0", "0", "0", "N", "0", "0", "0"},
+            {"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+            {"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+            {"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+            {"0", "0", "0", "0", "0", "0", "0", "N", "0", "0"},
+            {"0", "0", "N", "0", "0", "0", "0", "N", "0", "0"},
+            {"0", "N", "0", "0", "0", "0", "0", "N", "0", "0"},
+            {"N", "0", "0", "0", "0", "0", "0", "0", "0", "0"}
+    };
+    
+        // Exibir o tabuleiro
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+             printf("%s ", tabuleiro[i][j]);
+        }
+        printf("\n");
+        }
+        
+        return 0;
+    }    
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
@@ -35,6 +50,3 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
-
-    return 0;
-}
